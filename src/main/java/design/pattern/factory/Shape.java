0952904e090508f01a819492436eaf8773610ae8
mahongsheng <mahongsheng_cs@163.com>
2021-09-07 14:12:49 +0800

    初始化项目

---
+++ src/main/java/design/pattern/factory/Shape.java
@@ -0,0 +1,5 @@
+package design.pattern.factory;
+
+public interface Shape {
+    void draw();
+}
---
+++ src/main/java/design/pattern/factory/Square.java
@@ -0,0 +1,9 @@
+package design.pattern.factory;
+
+public class Square implements Shape {
+
+    @Override
+    public void draw() {
+        System.out.println("Inside Circle::draw() method.");
+    }
+}
